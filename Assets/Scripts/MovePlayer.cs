d6634dc6b49de7362c848b65cc9c9935d5e79bf6
Luis Garcia <garcialuis1998@yahoo.es>
2019-10-09 20:39:32 -0400

    Initial commit.

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovePlayer : MonoBehaviour
+{
+    private RaycastHit hit;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        // If user clicks on screen.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Send raycast from screen to mouse position.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
